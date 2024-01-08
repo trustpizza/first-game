ee7efb7f1aae7a0f41bf06f6f3d6627efcf1bd43
Axel <maxelolsson@gmail.com>
2024-01-08 10:16:08 -0500

    Get main character to work with simple animations

--- scenes/main_character.cs
+++ scenes/main_character.cs
@@ -4,7 +4,7 @@ using System;
 public partial class main_character : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float JumpVelocity = -450.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -15,18 +15,34 @@ public partial class main_character : CharacterBody2D
 		sprite2d = GetNode<AnimatedSprite2D>("Sprite2D");
 	}
  
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		// Animations
 
+		if (Math.Abs(velocity.X) > 1)
+		{
+			GD.Print("Running");
+			sprite2d.Animation = "running";
+		}
+		else
+		{
+			sprite2d.Animation = "idle";
+		}
 		// Add the gravity.
-		if (!IsOnFloor())
+		if (!IsOnFloor()) 
+		{
 			velocity.Y += gravity * (float)delta;
-
+			sprite2d.Animation = "jumping";
+		}
+		// int jumpCount = 0;
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			// jumpCount += 1;
 			velocity.Y = JumpVelocity;
-
+		// else if (Input.IsActionJustPressed("ui_accept") && jumpCount < 2)
+		// 	velocity.Y = JumpVelocity;
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
@@ -44,6 +60,6 @@ public partial class main_character : CharacterBody2D
 
 		bool isLeft = velocity.X < 0;
 		sprite2d.FlipH = isLeft;
-	
+
 	}
 }
